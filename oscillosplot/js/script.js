5a86c8fd89b81d6f51469c36d1005c3000019745
e-labinnovations <mohammedashad23@gmail.com>
2022-10-09 21:08:24 +0530

    drawing feature added

--- oscillosplot/js/script.js
+++ oscillosplot/js/script.js
@@ -21,9 +21,91 @@ var sprite = {
   Ys: []
 }
 
+var penColor = "black",
+  penThickness = 2;
+var flag = false,
+  prevX = 0,
+  currX = 0,
+  prevY = 0,
+  currY = 0,
+  dot_flag = false;
+
 accuracy.addEventListener('change', e => {
   accuracyDisplay.innerText = e.target.value;
 })
+
+function init() {
+  canvas.addEventListener("mousemove", function (e) {
+      findxy('move', e)
+  }, false);
+  canvas.addEventListener("mousedown", function (e) {
+      findxy('down', e)
+  }, false);
+  canvas.addEventListener("mouseup", function (e) {
+      findxy('up', e)
+  }, false);
+  canvas.addEventListener("mouseout", function (e) {
+      findxy('out', e)
+  }, false);
+}
+
+function draw() {
+  ctx.beginPath();
+  ctx.moveTo(prevX, prevY);
+  ctx.lineTo(currX, currY);
+  ctx.strokeStyle = penColor;
+  ctx.lineWidth = penThickness;
+  ctx.stroke();
+  ctx.closePath();
+}
+
+function clearCanvas() {
+  var m = confirm("Want to clear");
+  if (m) {
+      ctx.clearRect(0, 0, sprite.width, sprite.height);
+  }
+}
+
+function findxy(res, e) {
+  if (res == 'down') {
+      prevX = currX;
+      prevY = currY;
+      currX = e.clientX - canvas.offsetLeft;
+      currY = e.clientY - canvas.offsetTop;
+
+      flag = true;
+      dot_flag = true;
+      if (dot_flag) {
+          ctx.beginPath();
+          ctx.fillStyle = x;
+          ctx.fillRect(currX, currY, 2, 2);
+          ctx.closePath();
+          dot_flag = false;
+      }
+  }
+  if (res == 'up' || res == "out") {
+      flag = false;
+  }
+  if (res == 'move') {
+      if (flag) {
+          prevX = currX;
+          prevY = currY;
+          currX = e.clientX - canvas.offsetLeft;
+          currY = e.clientY - canvas.offsetTop;
+          draw();
+      }
+  }
+}
+
+const selectEraser = () => {
+  penColor = "white",
+  penThickness = 14;
+}
+
+const selectPencil = () => {
+  penColor = "black",
+  penThickness = 2;
+}
 
 const upload = () => {
   var img = new Image;
